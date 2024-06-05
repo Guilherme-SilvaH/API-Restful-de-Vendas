c2a8931a2de3ef92a1184c57c9715e70b2cc65ee
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-05 20:01:28 -0300

    coneção-DB

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,9 @@
-import 'reflex-metadata'
-import express, {NextFunction, Request, Response} from 'express'
-import cors from 'cors'
-import routes from './routes'
-import AppError from './errors/AppError';
-import '@shared/typeorm'
-
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '../errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -13,24 +12,22 @@ app.use(express.json());
 
 app.use(routes);
 
-
-//midllare
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError){
+  if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: 'error',
       message: error.message,
-    })
+    });
   }
 
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error'
-  })
-})
+    message: 'Internal server error',
+  });
+});
 
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
 
-app.listen(3333, ()=>{
-  console.log('Server start on port 3333!');
-})
 

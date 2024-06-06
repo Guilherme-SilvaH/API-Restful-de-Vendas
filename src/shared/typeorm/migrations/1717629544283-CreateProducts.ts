1c35e2df518fc5958f750e145e64b39510f272fe
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-05 21:09:34 -0300

    migrations-on

--- src/shared/typeorm/migrations/1717629544283-CreateProducts.ts
+++ src/shared/typeorm/migrations/1717629544283-CreateProducts.ts
@@ -1,49 +1,54 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateProducts1717629544283 implements MigrationInterface {
-      // Aqui vamos criar o modelo da tabale products
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(
-        new Table({
-        name: 'products',
-        columns:[{
-          name: 'id',
-          type: 'uuid',
-          isPrimary: true,
-          generationStrategy: 'uuid',
-          default: 'uuid_generate_v4'
-        },
-        {
-          name: 'name',
-          type: 'varchar',
-        },
-        {
-          name: 'price',
-          type: 'decimal',
-          precision: 10,
-          scale: 2,
-        },
-        {
-          name: 'quantity',
-          type: 'int'
-        },
-        {
-          name: 'created_at',
-          type: 'timestamp with time zone',
-          default: 'now()',
-        },
-        {
-          name: 'updated_at',
-          type: 'timestamp with time zone',
-          default: 'now()',
-        },
-
-      ]
-      }))
+        await queryRunner.createTable(
+            new Table({
+                name: "products",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "uuid",
+                        default: "uuid_generate_v4()",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        isNullable: false,
+                    },
+                    {
+                        name: "price",
+                        type: "decimal",
+                        precision: 10,
+                        scale: 2,
+                        isNullable: false,
+                    },
+                    {
+                        name: "quantity",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp with time zone",
+                        default: "now()",
+                        isNullable: false,
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp with time zone",
+                        default: "now()",
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable('products')
+        await queryRunner.dropTable("products");
     }
-
 }

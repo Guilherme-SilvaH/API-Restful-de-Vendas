9c2b93faed9baf6709c87371cfb78b95b363adfd
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-12 16:50:45 -0300

    Repositorio-Product

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, Repository } from "typeorm";
+import Product from "../entities/Product";
+
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product>{
+//Busca em Product o name, o criterio a ser buscado é o o name que foi passado no parametro
+  public async findByName(name: string): Promise<Product | undefined>{
+    const product = this.findOne({
+      where: {
+        name,
+      }
+
+    });
+    return product;
+  }
+}

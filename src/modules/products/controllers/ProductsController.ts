b8be968170ca840b52f8e7711775c999f094dd40
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-12 18:09:16 -0300

    showController

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from "express";
 import ListProductService from "../services/ListProductService";
+import ShowProductService from "../services/ShowProductService";
 
 export default class ProductsController {
 
-    public async index(request: Request, response: Response): Promise<Response> {
+
+    //controller para listar os produtos
+    public async index(request: Request, response: Response): Promise<Response>{
 
         const listProducts = new ListProductService();
 
         const products = await listProducts.execute();
 
         return response.json(products);
+    }
+
+    //controller para mostrar produtos atraves do ID
+    public async show(request: Request, response: Response): Promise<Response>{
+      const { id } = request.params;
+
+      const showProduct = new ShowProductService();
+
+      const product = showProduct.execute({ id });
+
+      return response.json(product);
+
     }
 }

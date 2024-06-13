7eb937e53677c6ff1e77697bf933a485f5b035fb
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-13 18:27:48 -0300

    router-index-main

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,10 @@
+import productsRouter from "@modules/products/routes/products.routes";
 import { Router } from "express";
 
 
 const routes = Router();
 
-routes.get('/', (request,response) => {
-  return response.json({message: "Hello Dev!"})
-})
-
+routes.use('/products', productsRouter);//vamos usar essa rota para acessar nossa RouterProducts, exemplo, get/products/:id
 
 export default routes
 

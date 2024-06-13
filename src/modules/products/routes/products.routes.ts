3fe7b7cbd753cff2699c207d7c516c547b888445
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-13 18:59:19 -0300

    celebrateCONFIGall

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -6,8 +6,12 @@ import ProductsController from "../controllers/ProductsController"
 const productsRouter = Router()//instancia para pegar os metodos dos ROUTER
 const productsController = new ProductsController();
 
+
+
+//Rota para listar todos os produtos
 productsRouter.get('/', productsController.index);
 
+//Rota para listar um produto atraves de um ID do produto
 productsRouter.get(
   '/:id',
   celebrate({
@@ -15,8 +19,10 @@ productsRouter.get(
       id: Joi.string().uuid().required(),
     },
   }),
-  productsController.show);
+  productsController.show
+);
 
+//Rota para Criar um produto
 productsRouter.post(
   '/',
   celebrate({
@@ -26,11 +32,38 @@ productsRouter.post(
       quantity: Joi.number().required()
     }
   }),
-  productsController.create);
+  productsController.create
+);
 
-productsRouter.put('/:id', productsController.update);
 
-productsRouter.delete('/:id', productsController.delete);
+//Rota para Atualizar um produto
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    },
+
+    [Segments.PARAMS]:{
+      id: Joi.string().uuid().required(),
+    },
+  }),
+productsController.update
+);
+
+
+//Rota para Deletar um produto
+productsRouter.delete('/:id',
+  celebrate({
+    [Segments.PARAMS]:{
+      id: Joi.string().uuid().required(),
+    },
+  }),
+
+  productsController.delete
+);
 
 
 export default productsRouter;

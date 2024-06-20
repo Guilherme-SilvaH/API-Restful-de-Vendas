9f94eaf7e03d0dfcce5a628fe1860493f631c828
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-20 16:14:13 -0300

    ShowUser - Service

---
+++ src/modules/users/typeorm/services/ShowUserService.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from "typeorm";
+import User from "../entities/User";
+import UsersRepository from "../repositories/UsersRepository";
+import AppError from "@shared/errors/AppError";
+
+
+
+interface IRequest {
+  id: string;
+}
+
+
+class ShowUserService{
+    public async execute({id}: IRequest): Promise<User>{
+        const UserRepository = getCustomRepository(UsersRepository);  //Utilizamos o getCustomRepository quando ja temos um repositorio customizado, se nao, usamos o getRepository
+
+        const user = await UserRepository.findOne(id);
+
+        //verificars se ja existe o produco com o name passado
+        if(!user){
+          throw new AppError('Product not found')
+        }
+
+        return user;
+    }
+}
+
+export default ShowUserService;

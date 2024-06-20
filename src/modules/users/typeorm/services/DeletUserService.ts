7158d38a8b7131b75dad765d4429bbb585b7e674
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-06-20 16:13:52 -0300

    DeletUser-Service

---
+++ src/modules/users/typeorm/services/DeletUserService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import UsersRepository from "../repositories/UsersRepository";
+
+
+interface IRequest {
+  id: string;
+}
+
+
+class DeleteUserService{
+    public async execute({id}: IRequest): Promise<void>{
+        const usersRepository = getCustomRepository(UsersRepository);  //Utilizamos o getCustomRepository quando ja temos um repositorio customizado, se nao, usamos o getRepository
+
+        const user = await usersRepository.findOne(id);
+        //verificars se ja existe o produco com o name passado
+        if(!user){
+          throw new AppError('Product not found')
+        }
+        await usersRepository.remove(user);
+    }
+}
+
+export default DeleteUserService;

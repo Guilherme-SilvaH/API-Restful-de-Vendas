80af17867411f2f6be349618e1271c16ba9aece7
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-07-10 20:20:46 -0300

    Customers-Finish

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from "@modules/users/routes/users.routes";
 import passwordRouter from "@modules/users/routes/password.routes";
 import { Router } from "express";
 import profileRouter from "@modules/users/routes/profile.routes";
+import CustumersRouter from "@modules/customers/routes/customers.routes";
 
 
 const routes = Router();
@@ -13,6 +14,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter)
+routes.use('/customers', CustumersRouter)
 
 export default routes
 

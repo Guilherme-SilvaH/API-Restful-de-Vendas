812d290e9d773919dfb9a9daf4e7c6b07003afd3
Guilherme Silva <guihenriquesilva10@gmail.com>
2024-08-21 20:24:13 -0300

    Edit

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,19 +1,36 @@
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, In, Repository } from "typeorm";
 import Product from "../entities/Product";
+
+
+interface IFindProducts { 
+  id: string
+}
 
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product>{
 //Busca em Product o name, o criterio a ser buscado é o o name que foi passado no parametro
-  public async findByName(name: string): Promise<Product | undefined>{
-    const product = this.findOne({
-      where: {
-        name,
-      }
+public async findByName(name: string): Promise<Product | undefined> {
+  const product = await this.findOne({
+    where: {
+      name,
+    },
+  });
+  return product || undefined;
+}
 
-    });
-    return product;
-  }
+
+
+public async findAllByIds (products: IFindProducts[],): Promise<Product[]> {
+  const productIds = products.map(product => product.id);
+
+  const existsProdutcs = await this.find({
+    where: {
+      id: In(productIds)
+    }
+  })
+  return existsProdutcs;
+}
 }
 
 export default ProductRepository;

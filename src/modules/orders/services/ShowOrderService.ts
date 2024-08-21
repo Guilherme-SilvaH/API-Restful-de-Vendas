7fd0dfcb61afd18c8a1cf918fbb62ea0b0d175ec
Guilherme Silva <guihenriquesilva10@gmail.com>
2024-08-21 20:51:05 -0300

    ShowOrder

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm";
+import Order from "../typeorm/entities/Order";
+import { OrdersRepository } from "../typeorm/repositories/OrderRepository";
+import AppError from "@shared/errors/AppError";
+
+
+interface IProduct {
+    id: string;
+    quantity: number
+}
+
+
+interface IRequest {
+    id: string
+}
+
+
+class ShowOrderService{
+    public async execute({ id}: IRequest): Promise<Order>{
+        const ordersRepository = getCustomRepository(OrdersRepository);  //Utilizamos o getCustomRepository quando ja temos um repositorio customizado, se nao, usamos o getRepository
+     
+        const order = await ordersRepository.findById(id);
+        if(!order){
+            throw new AppError('Order not Found',)
+    
+        };
+        return order
+
+    };
+ 
+}
+export default ShowOrderService;
